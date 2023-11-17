9e297330df20decd56011f2c974e81469f9efeb8
LinaHnidenko <hnidenko99@gmail.com>
2023-11-17 20:47:08 +0200

    Revert "changes"
    This reverts commit dc589e558a072c2162cb7b93cb6d203a9d0593b2.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,8 +15,6 @@ import MenuIcon from '@mui/icons-material/Menu';
 import AdbIcon from '@mui/icons-material/Adb';
 import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import { useDispatch, useSelector } from 'react-redux';
-import { logOut } from 'redux/auth/operations';
 
 const pages = ['Home', 'Contacts', 'SignUp'];
 
@@ -33,9 +31,6 @@ const theme = createTheme({
 });
 
 export const Header = () => {
-  const user = useSelector(state => state.auth.user);
-  const isAuth = useSelector(state => state.auth.token);
-  const dispatch = useDispatch();
   const [anchorElNav, setAnchorElNav] = useState(null);
 
   const handleOpenNavMenu = event => {
@@ -46,9 +41,6 @@ export const Header = () => {
     setAnchorElNav(null);
   };
 
-  const handleLogOut = () => {
-    dispatch(logOut());
-  };
   return (
     <ThemeProvider theme={theme}>
       <AppBar position="static" sx={{ mb: 20 }}>
@@ -148,41 +140,14 @@ export const Header = () => {
               ))}
             </Box>
 
-            {/* <Box sx={{ flexGrow: 0 }}>
+            <Box sx={{ flexGrow: 0 }}>
               <Button
                 onClick={handleCloseNavMenu}
                 sx={{ my: 2, color: 'white', display: 'block' }}
               >
                 <NavLink to="/login">Login</NavLink>
               </Button>
-            </Box> */}
-            {isAuth ? (
-              <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  {user.email}
-                </Button>
-                <NavLink to={'/'}>
-                  <Button
-                    onClick={handleLogOut}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
-                  >
-                    Log Out
-                  </Button>
-                </NavLink>
-              </Box>
-            ) : (
-              <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-                <Button
-                  onClick={handleCloseNavMenu}
-                  sx={{ my: 2, color: 'white', display: 'block' }}
-                >
-                  <NavLink to={'/login'}>Login</NavLink>
-                </Button>
-              </Box>
-            )}
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,18 +1,12 @@
 import { Button, TextField } from '@mui/material';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { logIn } from 'redux/auth/operations';
 
 export const Login = () => {
-  const isAuth = useSelector(state => state.auth.token);
   const navigate = useNavigate();
 
   const dispatch = useDispatch();
-
-  useEffect(() => {
-    isAuth && navigate('/contacts');
-  }, [isAuth, navigate]);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -22,7 +16,7 @@ export const Login = () => {
         password: e.target.elements.password.value,
       })
     );
-    // navigate('/contacts');
+    navigate('/contacts');
   };
 
   return (
